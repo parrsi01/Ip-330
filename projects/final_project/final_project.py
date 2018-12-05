7db59babc0bf5bd065aa9dc899e1d5f91654b0cb
Simon Parris <parrsi01@luther.edu>
2018-12-04 21:28:01 -0500

    final project additions

--- projects/final_project/final_project.py
+++ projects/final_project/final_project.py
@@ -1,12 +1,15 @@
 import os
 import requests
 from flask import Flask, redirect, url_for, make_response, session, escape, request, render_template, send_from_directory
-import psycopg2
-import records
+
 
 app = Flask(__name__)
 
+# Set the secret key to some random bytes.
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+
+'''
 def get_data_from_db(query: str) -> list:
     try:
         conn = psycopg2.connect(
@@ -17,22 +20,41 @@ def get_data_from_db(query: str) -> list:
     cur.execute(query)
     rows = cur.fetchall()
     return rows
+'''
 
-
+@app.route('/register', methods=['GET','POST'])
+def register():
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    pass
+    if 'username' in session:
+        return render_template('index.html', username=escape(session['username']))
+    else:
+        return render_template('login.html')
 
-@app.route('/main', methods=['GET','POST'])
-def main():
-    pass
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+       
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
-@app.route('/final', methods=['GET','POST'])
-def final():
-    pass
+@app.route('/player', methods=['GET','POST'])
+def player():
+    return render_template('player.html')
+
+@app.route('/league', methods=['GET','POST'])
+def league():
+    return render_template('league.html')
+
+@app.route('/team', methods=['GET','POST'])
+def team():
+    return render_template('team.html')
+
 
 
 
